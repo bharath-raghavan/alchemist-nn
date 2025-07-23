gradient not flowing thru coord_diff fixed

alchemist/nn/energy/dist.py
@@ -21,11 +21,11 @@ def get_periodic_images_within(pos, box, r_cut, batch):
     
     return pos_all_periodic_images, id_mapping, batch
 
-class Edges:
+class Edges(torch.nn.Module):
     def __init__(self, index, coord_diff):
         self.row = index[0]
         self.col = index[1]
-        self.dist = torch.sum((coord_diff)**2, 1).unsqueeze(1)
+        self.coord_diff = coord_diff
 
 class PeriodicDistance(torch.nn.Module):
     def __init__(

alchemist/nn/node/egcl.py
@@ -62,11 +62,12 @@ class EGCL(nn.Module):
         out = self.node_nn(agg)
         return out
             
-    def forward(self, h, edges):                
+    def forward(self, h, edges): 
+        radial = torch.sum((edges.coord_diff)**2, 1).unsqueeze(1)
         row = edges.row
         col = edges.col
         
-        edge_attr = self.edge_model(h[row], h[col], edges.dist)
+        edge_attr = self.edge_model(h[row], h[col], radial)
         
         return self.node_model(h, row, edge_attr)
             